319702cc278523d87ebaa9673ade20a8b4979539
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-01-15 18:40:34 +0100

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,8 +6,10 @@ public static class ReleaseNotes
 {
     internal static readonly Dictionary<string, string> Notes = new()
     {
-        {"2.2.3.2", "Multi-Sim"+
-                    "\n - DSX: FFB is remade from scratch, it is now 2 effects, feedback and vibration, which together will feel a lot more nuanced. Additionaly there are new options to customize the FFB."
+        {"2.2.3.2", "Multi-Sim:"+
+                    "\n - DSX: FFB is remade from scratch, it is now 2 effects, feedback and vibration, which together will feel a lot more nuanced. Additionaly there are new options to customize the FFB."+
+                    "\n\nAssetto Corsa Competizione:"+
+                    "\n - Speedometer HUD: Added option to increase RefreshRate up to 60 Hz"
                     },
         {"2.2.3.0", "Race Element:"+
                     "\n- Updated Start Screen HUD."+
