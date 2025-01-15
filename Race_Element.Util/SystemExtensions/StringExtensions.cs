b345aec9c50a11bce563b4c4c66dba3a5e4e3bd8
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-01-15 18:58:20 +0100

    Update StringExtensions.cs

--- Race_Element.Util/SystemExtensions/StringExtensions.cs
+++ Race_Element.Util/SystemExtensions/StringExtensions.cs
@@ -1,41 +1,25 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace RaceElement.Util.SystemExtensions;
 
 public static class StringExtensions
 {
-    public static string FillStart(this String text, int maxLength, char filler)
+    public static string FillStart(this string value, int maxLength, char filler)
     {
-        int length = text.Length;
-
-        if (length < maxLength)
-        {
-            int missing = maxLength - length;
-            for (int i = 0; i < missing; i++)
-                text = filler + text;
-        }
-
-        return text;
+        if (string.IsNullOrEmpty(value)) return new string(filler, maxLength);
+        return value.PadLeft(maxLength, filler);
     }
 
-    public static string FillEnd(this String text, int maxLength, char filler)
+    public static string FillEnd(this string value, int maxLength, char filler)
     {
-        int length = text.Length;
-
-        if (length < maxLength)
-        {
-            int missing = maxLength - length;
-            for (int i = 0; i < missing; i++)
-                text += filler;
-        }
-
-        return text;
+        if (string.IsNullOrEmpty(value)) return new string(filler, maxLength);
+        return value.PadRight(maxLength, filler);
     }
-
 
     public static string ToString(this string[] values)
     {
+        if (values == null || values.Length == 0) return string.Empty;
+
         var builder = new StringBuilder();
         for (int i = 0; i < values.Length; i++)
         {
