135b34da6eb16d5798595f953e6dbfb5d35b58ae
kamih <guillaume.stordeur@gmail.com>
2025-01-25 16:28:49 -0700

    added ds5w_set_trigger_effect_feedback and ds5w_set_trigger_effect_weapon to DualSense API Resources

--- Race Element.HUD.Common/Overlays/Driving/DualSense/Resources.cs
+++ Race Element.HUD.Common/Overlays/Driving/DualSense/Resources.cs
@@ -4,12 +4,6 @@ namespace RaceElement.HUD.Common.Overlays.Driving.DualSense;
 
 internal static partial class Resources
 {
-    /*
-        DS5W_CAPI int ds5w_init(void);
-        DS5W_CAPI void ds5w_shutdown(void);
-        DS5W_CAPI int ds5w_set_trigger_effect_off(int left);
-        DS5W_CAPI int ds5w_set_trigger_effect_vibration(int left, int pos, int amp, int freq);
-    */
     [LibraryImport("ds5w_x64.dll", SetLastError = true)]
     public static partial int ds5w_init();
 
@@ -21,4 +15,10 @@ internal static partial class Resources
 
     [LibraryImport("ds5w_x64.dll", SetLastError = true)]
     public static partial int ds5w_set_trigger_effect_vibration(int left, int pos, int amp, int freq);
+
+    [LibraryImport("ds5w_x64.dll", SetLastError = true)]
+    public static partial int ds5w_set_trigger_effect_feedback(int left, int pos, int strength);
+
+    [LibraryImport("ds5w_x64.dll", SetLastError = true)]
+    public static partial int ds5w_set_trigger_effect_weapon(int left, int start, int end, int strength);
 }
