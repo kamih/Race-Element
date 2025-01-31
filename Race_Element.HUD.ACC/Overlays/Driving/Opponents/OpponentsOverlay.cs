7f8feba47704267022fe4b7556b3aa882b7a066a
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-01-31 17:44:46 +0100

    Create OpponentsOverlay.cs
    add base for the opponents overlay

---
+++ Race_Element.HUD.ACC/Overlays/Driving/Opponents/OpponentsOverlay.cs
@@ -0,0 +1,38 @@
+﻿using RaceElement.Data.ACC.EntryList;
+using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.Opponents;
+[Overlay(
+Name = "Opponents",
+Description = "Shows information about the cars ahead and behind in terms of race position.")]
+internal sealed class OpponentsOverlay : AbstractOverlay
+{
+    private readonly OpponentsConfiguration _config = new();
+    private class OpponentsConfiguration : OverlayConfiguration
+    {
+
+    }
+
+    public OpponentsOverlay(Rectangle rectangle) : base(rectangle, "Opponents")
+    {
+    }
+
+    public sealed override void Render(Graphics g)
+    {
+        var allCars = EntryListTracker.Instance.Cars;
+        if (allCars.Count > 0)
+        {
+            var playerCar = allCars.FirstOrDefault(x => x.Key == pageGraphics.PlayerCarID);
+            if (playerCar.Value == null) return;
+
+            int playerPosition = playerCar.Value.RealtimeCarUpdate.Position;
+        }
+    }
+}
