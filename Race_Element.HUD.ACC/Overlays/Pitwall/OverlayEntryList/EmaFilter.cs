94f0a0dd654c79bdc6651d25c77d1b2031b902d2
RiddleTime <reinierklarenberg@gmail.com>
2025-05-27 18:57:30 +0200

    Update EmaFilter.cs
    Set class to "internal" visually

--- Race_Element.HUD.ACC/Overlays/Pitwall/OverlayEntryList/EmaFilter.cs
+++ Race_Element.HUD.ACC/Overlays/Pitwall/OverlayEntryList/EmaFilter.cs
@@ -1,7 +1,7 @@
 ﻿// https://en.wikipedia.org/wiki/Exponential_smoothing
 namespace RaceElement.HUD.ACC.Overlays.OverlayDebugInfo.OverlayEntryList;
 
-class EmaFilter(int period, double initialValue)
+internal class EmaFilter(int period, double initialValue)
 {
     private double _value = initialValue;
     private readonly double _alpha = 2.0 / (period + 1);
